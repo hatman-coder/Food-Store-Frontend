851d722045c8108718eff1746be6d23cd2c63e45
Elliot <elliot.59@outlook.com>
2022-10-15 15:58:52 +0600

    optimized code and made div row responsive

--- src/components/body/Products.js
+++ src/components/body/Products.js
@@ -1,74 +1,74 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import './style/style.css'
 
 
 const Product = () => {
 
     const [data, setData] = useState([])
     const [error, setError] = useState('')
-    
+
 
     const loadAllProducts = () => {
-        axios.get('http://127.0.0.1:8000/product/').then( (res) => {
-            if(res.error){
+        axios.get('http://127.0.0.1:8000/product/').then((res) => {
+            if (res.error) {
                 setError(res.error)
                 console.log(error)
-            }
-            else {
+            } else {
                 setData(res.data)
 
-            }   
-   
+            }
+
         })
     }
     // eslint-disable-next-line
-    useEffect(() => {loadAllProducts()}, [])
+    useEffect(() => {
+        loadAllProducts()
+    }, [])
 
 
     const ButtonHandler = (e) => {
-    
-        return  console.log({id: e.id, product: e.name})
-        
-    }
+        return console.log({id: e.id, product: e.name})
 
-    const CheckBoxHandler =() => {
-        return console.log('CheckBox Clicked')
+    }
+    // eslint-disable-next-line
+    const CheckBoxHandler = (e) => {
+        return(console.log(document.getElementById('CheckBoxLabel')))
     }
 
     return (
-        <div >
-                {data.map((item, index )=> 
-                    {
-                        return(
-                            <div className="container" key={index} style={{alignItems: 'center', display: 'block', alignSelf: 'center'}}>
-                                <div className="row" >
-                                    <div className="col-6">
-                                        <img src={item.img} alt='Cannot Load Property' style={{ maxWidth:'300px',paddingTop: '5px', maxHeight: '160px', minWidth: '250px'}}/>
-                                        <h2 style={{fontSize: '1.5rem'}}>{item.name}</h2>
-                                        <b><p style={{color: 'red', fontWeight: '3rem'}}>Price: {item.price}</p></b>
-                                        {item.addOnes.map((item, index )=> {
-                                            return(
-                                                <div key={index} className="form-check form-check-inline">
-                                                    <input className="form-check-input" onClick={() => {CheckBoxHandler()}} type="checkbox" id="inlineCheckbox}" value="option" />
-                                                        <label htmlFor="inlineCheckbox" className="form-check-label">{item}</label>
-                                                </div>
-                                            )
-                                        })}
-                                        <br></br>
-                                        <br></br>
-                                        <button style={{height: '40px', width: '150px'}} type="submit" onClick={() => ButtonHandler(item)} className="btn btn-dark">Add to cart</button>
-                                        <br></br>
-                                        <br></br>
+        <div className='container' style={{ paddingTop: '3rem'}}>
+            <div className='row row-centered pos'>
+                {data.map((item, index) => {
+                    return (
+                        <div key={index} className='col-lg-4 col-md-8 col-sm-12 text-center'>
+                            <div className="card text-white  mb-3 ">
+                                <img className="card-img-top" src={item.img} alt="Unavailable"/>
+                                <div className="card-body transbox">
+                                    <h5 className="card-title">{item.name}</h5>
+                                    <p className="card-text">Price: {item.price}</p>
+                                    {item.addOnes.map((getAddones, index) => {
+                                        return(
+                                            <div key={index} className='form-check form-check-inline'>
+                                                <input className='form-check-input' onClick={() => CheckBoxHandler({index})} type='checkbox' id='inlineCheckbox'/>
+                                                <label htmlFor='inlineCheckbox' id='CheckBoxLabel'>{getAddones}</label>
+                                            </div>
+                                        )
+                                    })}
+                                    <br></br>
+                                    <br></br>
+                                    <div className='card-footer'>
+                                        <button className="myButton" onClick={() => ButtonHandler(item)}>Add To Cart</button>
                                     </div>
                                 </div>
                             </div>
-                        )
-                    }
-                    )}
-    
+                        </div>
+                    )
+                })}
+            </div>
         </div>
-     
+
+
     )
 }
 
