38b58e2862166382f1d90e361e7738065a424e64
Elliot <elliot.59@outlook.com>
2022-10-16 00:53:18 +0600

    added check box data into the console log to check

--- src/components/body/Products.js
+++ src/components/body/Products.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import './style/style.css'
 
@@ -7,8 +7,9 @@ const Product = () => {
 
     const [data, setData] = useState([])
     const [error, setError] = useState('')
+    const myarray = useRef([])
 
-
+    // eslint-disable-next-line
     const loadAllProducts = () => {
         axios.get('http://127.0.0.1:8000/product/').then((res) => {
             if (res.error) {
@@ -21,44 +22,55 @@ const Product = () => {
 
         })
     }
+
     // eslint-disable-next-line
     useEffect(() => {
         loadAllProducts()
     }, [])
 
 
-    const ButtonHandler = (e) => {
-        return console.log({id: e.id, product: e.name})
-
-    }
     // eslint-disable-next-line
+    var a = []
     const CheckBoxHandler = (e) => {
-        return(console.log(document.getElementById('CheckBoxLabel')))
+        console.log(e.target.value)
+        myarray.current.push(e.target.value)
     }
+
+    // eslint-disable-next-line
+    const ButtonHandler = (e) => {
+        return console.log({id: e.id, product: e.name, get_add_ones: myarray.current.join(',')})
+    }
+
 
     return (
-        <div className='container' style={{ paddingTop: '3rem'}}>
+        <div className='container divBody'>
             <div className='row row-centered pos'>
                 {data.map((item, index) => {
                     return (
-                        <div key={index} className='col-lg-4 col-md-8 col-sm-12 text-center' style={{maxWidth: '480px'}}>
+                        <div key={index} className='col-lg-4 col-md-8 col-sm-12 text-center'
+                             style={{maxWidth: '480px'}}>
                             <div className="card text-white  mb-3 ">
                                 <img className="card-img-top" src={item.img} alt="Unavailable"/>
                                 <div className="card-body transbox">
                                     <h5 className="card-title">{item.name}</h5>
                                     <p className="card-text">Price: {item.price}</p>
-                                    {item.addOnes.map((getAddones, index) => {
-                                        return(
-                                            <div key={index} className='form-check form-check-inline'>
-                                                <input className='form-check-input' onClick={() => CheckBoxHandler({index})} type='checkbox' id='inlineCheckbox'/>
-                                                <label htmlFor='inlineCheckbox' id='CheckBoxLabel'>{getAddones}</label>
-                                            </div>
-                                        )
-                                    })}
+                                    <div className='checkBox'>
+                                        {item.addons.map((getAddons, index) => {
+                                            return (
+                                                <div key={index} className='form-check form-check-inline'>
+                                                    <input className='form-check-input'
+                                                           onClick={CheckBoxHandler} type='checkbox' value={getAddons}
+                                                    />
+                                                    <label htmlFor='inlineCheckbox'>{getAddons}</label>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
                                     <br></br>
                                     <br></br>
                                     <div className='card-footer'>
-                                        <button className="myButton" onClick={() => ButtonHandler(item)}>Add To Cart</button>
+                                        <button className="myButton" onClick={() => ButtonHandler(item)}>Add To Cart
+                                        </button>
                                     </div>
                                 </div>
                             </div>
