18e60d5e47dac510314fda033e0e737b6b81fe82
Elliot-59 <elliot.59@outlook.com>
2022-11-17 16:32:57 +0600

    fixed layout

--- src/components/body/Cart.js
+++ src/components/body/Cart.js
@@ -6,6 +6,8 @@ const Cart = () => {
 
    const [data, setData] = useState([])
 
+   
+
    const Total = (e) => {
     let price = parseInt(document.getElementById('price').innerHTML)
     let quantity = parseInt(document.getElementById('number').innerHTML)
@@ -14,17 +16,17 @@ const Cart = () => {
     return result
 }
 
+    
    const MinusHandle = (e) =>{
+            let number = parseInt(document.getElementById('number').innerHTML)
+            if (number !== 1){
+                number -= 1
+            }
+            document.getElementById('number').innerHTML = number  
+            Total()
 
-        let number = parseInt(document.getElementById('number').innerHTML)
-        if (number !== 1){
-            number -= 1
-        }
-        document.getElementById('number').innerHTML = number  
-        Total()
         }
         
-
     const PlusHandle = (e) =>{
         let number = parseInt(document.getElementById('number').innerHTML)
         number += 1
@@ -39,6 +41,7 @@ const Cart = () => {
         window.location.reload()
        
     }
+
 
    useEffect(() => {
     if(data.length === 0 ){
@@ -63,8 +66,10 @@ const Cart = () => {
                 <th scope="col">Image</th>
                 <th scope="col">Name</th>
                 <th scope="col">Price</th>
+                <th scope="col">Item</th>
                 <th scope="col">Quantity</th>
-                <th scope="col">total</th>
+                <th scope="col">Total</th>
+                <th scope="col">Delete</th>
                 </tr>
             </thead>
             {data.map((item, index) => {
@@ -90,17 +95,39 @@ const Cart = () => {
                                 <td id="price">
                                     {item.price}
                                 </td>
+                        
                                 <td>
-                                    <p id="number" >1</p>
-                                    <button id="minus" onClick={MinusHandle} value={item.id}>-</button> <span></span>
-                                    <button id="plus" onClick={PlusHandle} value={item.id}>+</button>
+                                <p id="number">1</p>
                                 </td>
+
+                                <td>
+                                <button id="minus" className="btn1" onClick={MinusHandle} value={item.id}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+                                        </svg>
+                                </button>
+                                &nbsp;
+                                &nbsp;
+                                &nbsp;
+                                <button id="plus" className="btn2" onClick={PlusHandle} value={item.id}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                        </svg>
+                                </button>
+                                </td>
+
 
                                 <td id="total">
                                     {item.price}
                                 </td>
+
                                 <td >
-                                    <button onClick={() => DeleteHandle(item.id)}>Delete</button>
+                                    <button className="btn3" onClick={() => DeleteHandle(item.id)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                        </svg>
+                                    </button>
                                 </td>
 
                             </tr>
